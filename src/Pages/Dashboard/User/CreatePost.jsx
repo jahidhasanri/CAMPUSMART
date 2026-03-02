0648d1d2f643c94d3620bb45410d338fe4114388
Abu Nayeem Riyad <rionimo5@gmail.com>
2026-03-03 03:52:11 +0600

    routes created for users->order,wishlist,create,posts

---
+++ src/Pages/Dashboard/User/CreatePost.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreatePost = () => {
+  return (
+    <div>
+      create post page ..............
+    </div>
+  );
+};
+
+export default CreatePost;
---
+++ src/Pages/Dashboard/User/MyOrders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyOrders = () => {
+    return (
+        <div>
+            orders page............
+        </div>
+    );
+};
+
+export default MyOrders;
